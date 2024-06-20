4a798c4f933a31348f2906ca175a975e7292bf90
WalnutRavi <140723920+WalnutRavi@users.noreply.github.com>
2024-06-20 16:39:55 -0300

    aula 105

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ Webcam.set({
 });
 camera=document.getElementById("Webcam")
 Webcam.attach(camera);
+function Take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("Result").innerHTML='<img id="img_capture" src="'+data_uri+'"/>'
+    })
+}
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1GkxKd8iZ/model.json",model_loaded)
+function model_loaded(){
+    console.log("modelo carregado")
+}
+function Check(){
+    img=document.getElementById("img_capture")
+    classifier.classify(img,gotResult)
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error)
+    }
+else{
+    //console.log(results)
+    document.getElementById("Object_result").innerHTML=results[0].label
+    document.getElementById("Object_accuracy").innerHTML=results[0].confidence.toFixed(3)
+}    
+}
